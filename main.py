c8a4a64a11e0c358bd58c2564bc7533c61c8dbf0
Namnarak <nutdanai.namo2@gmail.com>
2026-09-07 23:17:07 +0700

    hotfix: install dashboard security middleware

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 from bot import Cyori
 from utils.config import BOT_TOKEN
+from utils.dashboard_security import make_dashboard_security_middleware
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("discord.http").setLevel(logging.CRITICAL)
@@ -19,11 +20,17 @@ async def get_prefix(bot, message: discord.Message):
         
     return commands.when_mentioned_or(prefix)(bot, message)
 
+class SecuredCyori(Cyori):
+    async def setup_hook(self):
+        await super().setup_hook()
+        if self.web_app is not None:
+            self.web_app.middlewares.append(make_dashboard_security_middleware(self))
+
 intents = discord.Intents.default()
 intents.members = True
 intents.message_content = True
 
-bot = Cyori(
+bot = SecuredCyori(
     command_prefix=get_prefix,
     activity=discord.Activity(type=discord.ActivityType.listening, name="Starting..."),
     help_command=None,
